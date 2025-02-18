c999582f3b737334962089c69468d3ec3b4d27d2
Jonas Elenius Skovly Mikkelsen <au721413@uni.au.dk>
2025-02-18 09:58:12 +0100

    Restructured code to a src dir

--- src/resource_path.py
+++ src/resource_path.py
@@ -4,7 +4,9 @@ import sys
 
 def resource_path(relative_path):
     try:
+        # Checking if we are running from pyinstaller
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath(".")
+        # Else get path of dir which this file is in
+        base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, relative_path)
